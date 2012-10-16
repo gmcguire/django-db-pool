244fdf716631a25f6a719037e91c083b3106edbe
Greg McGuire <greg@buzzcar.com>
2012-10-16 10:39:57 -0400

    Bump to 0.0.7 for issue #3 (pool_reconnect)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='django-db-pool',
-    version='0.0.6',
+    version='0.0.7',
     author=u'Greg McGuire',
     author_email='gregjmcguire+github@gmail.com',
     packages=find_packages(),
